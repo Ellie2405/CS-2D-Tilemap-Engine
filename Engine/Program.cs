20925f2ec0a1c846f3a4240a8e6ab7db4772826e
Brayerr <brayer_alon@walla.com>
2023-01-19 22:04:07 +0200

    ffffffs

--- Engine/Program.cs
+++ Engine/Program.cs
@@ -3,8 +3,8 @@
 Vector2 pos = new Vector2(1, 1);
 RectangleTile recTile = new RectangleTile(pos);
 Tilemap map = new Tilemap(recTile, 8, 8);
-map.GetEnumerator();
-Console.WriteLine($"This map has {map.Count()} items.");
+
+
 
 //foreach (var item in map)
 //{
--- Engine/Tilemap.cs
+++ Engine/Tilemap.cs
@@ -9,11 +9,12 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    public class Tilemap : IEnumerable<Tile>
+    public class Tilemap
     {
+        public Tile[,] grid;
         public Tilemap(Tile tileType, int rows, int cols)
         {
-            Tile[,] grid = new Tile[rows, cols];
+            grid = new Tile[rows, cols];
             TileInjector(rows, cols, grid);
         }
 
@@ -36,36 +37,5 @@ namespace Engine
             }
             Log.InfoMessage("The tile map finished configuring.");
         }
-
-        public IEnumerator<Tile> GetEnumerator() => new TileMapEnumerator<Tile>();
-
-        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
-
-    }
-
-
-    public struct TileMapEnumerator<T> : IEnumerator<T>
-    {
-        T[] enumerables;
-        int index = -1;
-
-        public TileMapEnumerator(T[] enumerables)
-        {
-            this.enumerables = enumerables;
-        }
-
-        public T Current => index < enumerables.Length && index >= 0 ? enumerables[index] : default(T);
-
-        object IEnumerator.Current => Current;
-
-        public bool MoveNext()
-        {
-            index++;
-            return index < enumerables.Length;
-        }
-
-        public void Reset() { }
-
-        public void Dispose() { }
     }
 }
